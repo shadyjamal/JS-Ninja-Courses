23e42fa34485029c5ab0acfc1237624cfe06f426
Chadi Jamal <cjamal@e2r3p10.1337.ma>
2020-03-12 23:50:04 +0100

    added other array methos

--- Modern JS/Arrays methods/sandbox.js
+++ Modern JS/Arrays methods/sandbox.js
@@ -1,7 +1,5 @@
-const scores = [10, 30, 15, 60, 50, 20];
-
-// Filter an array
-
+// Filter an array / object
+const scores = [10, 30, 15, 60, 50, 20, 90, 70];
 const array = scores.filter((score) => {
     return score > 20;
 });
@@ -19,7 +17,7 @@ const filteredUser = users.filter(user => {
 
 console.log(filteredUser);
 
-// Map an array
+// Map an array / object
 
 const items = [
     {name: 'gold star', price: 20},
@@ -32,3 +30,60 @@ const items = [
   const mappedProds = items.filter(prods => prods.price > 30).map(prods => prods.price / 2);
 
   console.log(mappedProds);
+
+  // Reduce an array / Object
+
+  const result = scores.reduce((acc, curr) => {
+    curr > 50 ? acc++ : 0;
+    return acc;
+  }, 0);
+
+  console.log(result);
+
+  const gamescores = [
+    {player: 'mario', score: 50},
+    {player: 'yoshi', score: 30},
+    {player: 'mario', score: 70},
+    {player: 'crystal', score: 60},
+    {player: 'mario', score: 50},
+    {player: 'yoshi', score: 30},
+    {player: 'mario', score: 70},
+    {player: 'crystal', score: 60},
+    {player: 'mario', score: 50},
+    {player: 'yoshi', score: 30},
+    {player: 'mario', score: 70},
+    {player: 'crystal', score: 60}
+  ];
+
+  let gameresult = gamescores.reduce((acc, curr)=>{
+    if (curr.player === 'mario')
+        acc += curr.score;
+    return acc;
+  }, 0);
+  
+  console.log(gameresult);
+  
+  // Find in an array
+
+  const firstHighscore = scores.find(score => score > 50);
+  console.log(firstHighscore);
+
+  // Sort an array
+
+  const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
+
+  names.sort();
+  console.log(names);
+
+  scores.sort();
+
+  const items_to_sort = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50}
+  ];
+
+  items_to_sort.sort((a, b) => b.price - a.price);
+  console.log(items_to_sort);
